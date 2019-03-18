20f9809ccc7cd8255016a70b7960e1f67d8e7455
Andrey Filonov <andrey.filonov@gmail.com>
2019-03-18 19:30:22 +1300

    Initial Commit

---
+++ Wheely/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wheely
+//
+//  Created by Andrey Filonov on 18/03/2019.
+//  Copyright © 2019 Andrey Filonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
